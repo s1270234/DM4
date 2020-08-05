bc5feebff3e29495811891132b22ff6de65cc703
s1270234 <69234305+s1270234@users.noreply.github.com>
2020-08-05 18:22:46 +0900

    最新バージョン

---
+++ ex4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int i, n, heads=0, tails=0;
+  for(i=0;i<3;i++){
+    n = rand();
+    if(n%2==0){
+      printf("Round %d: Head\n", i+1);
+      heads++;
+    } else{
+      printf("Round %d: Tails\n",i+1);
+      tails++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n", heads,tails);
+  return 0;
+}
