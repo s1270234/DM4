fda0492c52d09cc991001cbba03181da7059a6e0
s1270234 <69234305+s1270234@users.noreply.github.com>
2020-08-06 16:20:46 +0900

    name part and coin part improved. Won or lost add

--- ex4_1.c
+++ ex4_1.c
@@ -2,17 +2,42 @@
 #include<stdlib.h>
 
 int main(){
-  int i, nu, heads=0, tails=0;
+  int i, n, heads=0, tails=0;
+
+  //name part
+  char str[8];
+
+  printf("What your name? ");
+
+  scanf("%s",str);
+
+  printf("Hi!%s",str);
+
+  printf("\n");
+
+  //coin part
   for(i=0;i<3;i++){
-    nu = rand();
-    if(nu%2==0){
+
+    n = rand();
+
+    if(n%2==0){
+
       printf("Round %d: Head\n", i+1);
       heads++;
+
     } else{
+
       printf("Round %d: Tails\n",i+1);
       tails++;
+
     }
   }
   printf("Heads: %d, Tails: %d\n", heads,tails);
+  //won or lost part
+  if(heads >=2){
+    printf("%s is won!",str);
+  }else{
+    printf("%s is lost.",str);
+  }
   return 0;
 }
